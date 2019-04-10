7a17f57a63d963267f67fbdc20608b510d7843d0
haythamdahri <haytham.dahri@gmail.com>
2019-04-10 23:23:00 +0100

    NEW ROUTING IMPROVMENTS

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import { ServersService } from '../servers.service';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
 
 @Component({
   selector: 'app-edit-server',
@@ -11,6 +11,7 @@ export class EditServerComponent implements OnInit {
   @Input() server: {id: number, name: string, status: string};
   @Input() serverName: string;
   @Input() serverStatus: string;
+  allowEdit: boolean  = false;
 
   constructor(
     private serversService: ServersService,
@@ -19,9 +20,20 @@ export class EditServerComponent implements OnInit {
   ngOnInit() {
     console.log(this.route.snapshot.queryParams);
     console.log(this.route.snapshot.fragment);
-    this.server = this.serversService.getServer(1);
+    this.route.queryParams.subscribe(
+      (params: Params) => {
+        this.allowEdit = params['allowEdit'] === '1' ? true : false;
+      }
+    );
+    const id = +this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
   }
 
   onUpdateServer() {
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -8,12 +9,35 @@ import { ServersService } from '../servers.service';
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
-  @Input('theServer') server: {id: number, name: string, status: string};
+  server: {id: number, name: string, status: string};
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute,
+    private router: Router
+    ) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    const id = +this.route.snapshot.params['id']; // id is a string and we convert it to a number using + symbol
+    this.server = this.serversService.getServer(id);
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.server = this.serversService.getServer(+params['id']);
+      }
+    );
+  }
+
+  onNextServer() {
+    // We are not using {relativeTo: this.route} because we need to change the passed param <<id>> from the root
+    // /servers/this.server.id
+    this.router.navigate(['/servers', this.server.id+1]);
+  }
+
+  editServer() {
+    // To preserve parameters sent from the parent component(Servers), we will add <<queryParamsHandling>> to handle this |
+    // 'preserve' to keep params
+    // 'merge' to override params
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve', preserveFragment: 'preserve'});
   }
 
 }
